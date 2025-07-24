b8866e89a850d7b34ef621cd8cbc1fcb21bc6f56
lucascampanharo <lucascampanharodev@gmail.com>
2025-07-24 11:59:41 -0300

    :style: title animation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,25 @@
 import "../styles/menu.css";
 
 export default function Menu({ aoIniciar }) {
+  const titulo = "Aventura Interativa";
+
   return (
     <div className="menu-container">
-      <h1 className="titulo-jogo">Aventura Interativa</h1>
-      <button className="botao-iniciar" onClick={aoIniciar}>
+      <h1 className="titulo-jogo">
+        {titulo.split("").map((letra, i) => (
+          <span key={i} style={{ animationDelay: `${i * 0.1}s` }}>
+            {letra === " " ? "\u00A0" : letra}
+          </span>
+        ))}
+      </h1>
+
+      <button className="botao" onClick={aoIniciar}>
         Iniciar do Jogo
       </button>
-      <button className="botao-iniciar" onClick={aoIniciar}>
+      <button className="botao" onClick={aoIniciar}>
         Opções
       </button>
-      <button className="botao-iniciar" onClick={aoIniciar}>
+      <button className="botao" onClick={aoIniciar}>
         Sair do Jogo
       </button>
     </div>
