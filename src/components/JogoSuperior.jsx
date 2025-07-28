6ac9c74ec5711a6524f2573b1a718e370b021152
lucascampanharo <lucascampanharodev@gmail.com>
2025-07-28 10:51:06 -0300

    new image background

--- src/components/JogoSuperior.jsx
+++ src/components/JogoSuperior.jsx
@@ -5,7 +5,7 @@ import Opcoes from "./Opcoes";
 export default function JogoSuperior({ cena, avancarCena }) {
   return (
     <div className="jogo-superior">
-      <h1 className="titulo-jogo">Aventura Interativa</h1>
+      <h1 className="titulo-jogo">Na Floresta Obscura</h1>
       <Texto html={cena.texto} />
       <Opcoes opcoes={cena.opcoes} onEscolher={avancarCena} />
     </div>
